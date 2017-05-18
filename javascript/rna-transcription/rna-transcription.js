daee62b062d60d49f538c071a15e63e0132b11b8
Administrator <administrator@Administrators-MacBook-Pro.local>
2017-05-18 10:11:46 -0400

    redo on the last commit

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,28 +1,23 @@
- let DnaTranscriber = function(){};
+let DnaTranscriber = function(){};
 
- DnaTranscriber.prototype.toRna = function(dna){
-   let dnArray = ['G', 'C', 'T', 'A'];
-   let rnArray = ['C', 'G', 'A', 'U'];
-   let rna = '';
+DnaTranscriber.prototype.toRna = function(dna){
+  let dnArray = ['G', 'C', 'T', 'A'];
+  let rnArray = ['C', 'G', 'A', 'U'];
+  let rna = '';
 
 
-   for (let i = 0; i<=dna.length; i++){
-     let errorCheck = 0;
-     for (let j = 0; j<=dnArray.length; j++){
-       errorCheck ++;
-       if(dna.charAt(i) === dnArray[j]){
-         console.info('Error check', errorCheck);
-         rna = rna.concat(rnArray[j]);
-         console.info(rna);
-       }
-     }
-   }
-   return rna;
+  for (let i = 0; i<=dna.length; i++){
+    let errorCheck = 0;
+    for (let j = 0; j<=dnArray.length; j++){
+      errorCheck ++;
+      if(dna.charAt(i) === dnArray[j]){
+        console.info('Error check', errorCheck);
+        rna = rna.concat(rnArray[j]);
+        console.info(rna);
+      }
+    }
+  }
+  return rna;
 };
 
 module.exports = DnaTranscriber;
-
-
-// if( error === 4 && dna.charAt(i) !== dnArray[j]) {
-//   throw new Error('Invalid input');
-//  }
